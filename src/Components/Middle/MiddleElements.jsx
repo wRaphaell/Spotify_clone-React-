33fc512dd68bd1180144ae92920b0aad62a9863f
Raphael Rattelli <raphael.rattelli@epitech.eu>
2024-07-23 14:47:50 +0200

    [ADD]: a bunch of elements in the middle part thanks to the file assets.js

--- src/Components/Middle/MiddleElements.jsx
+++ src/Components/Middle/MiddleElements.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './MiddleElements.scss'
 import firstCover from '../../assets/img1.jpg'
+import { PopRadiojs } from '../../assets/assets'
+import { Artistsjs } from '../../assets/assets'
+import { PopularAlbumsjs } from '../../assets/assets'
+import { PasManquer } from '../../assets/assets'
 
 export default function MiddleElements() {
   return (
@@ -13,28 +17,22 @@ export default function MiddleElements() {
             <span className="Title">Daily Mix 1</span>
             <span className="artists">Mitroglou</span>
           </div>
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
         </div>
       </div>
+
       <div className="PopularRadio">
-        <span className="Mid">Popular radio</span>
-        <div className="FullLign">
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
+            <span className="Mid">Popular radio</span>
+            <div className="FullLign">
+                {PopRadiojs.map((radio) => (
+                    <div className="SongContainer" key={radio.id}>
+                        <img src={radio.image} alt={radio.name} className="Cover"/>
+                        <span className="Title">{radio.name}</span>
+                        <span className="artists">{radio.desc}</span>
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
+      
       <div className="RecentlyPlayed">
         <span className="Mid">Recently played</span>
         <div className="FullLign">
@@ -43,56 +41,45 @@ export default function MiddleElements() {
             <span className="Title">Daily Mix 1</span>
             <span className="artists">Mitroglou</span>
           </div>
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
         </div>
       </div>
+
       <div className="PopularArtists">
         <span className="Mid">Popular artists</span>
         <div className="FullLign">
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
+          {Artistsjs.map((artistss) => (
+            <div className="SongContainer" key={artistss.id}>
+              <img src={artistss.image} alt="Img" className="CoverArtist"/>
+              <span className="Title">{artistss.name}</span>
+              <span className="artists">{artistss.desc}</span>
+            </div>
+          ))}
         </div>
       </div>
+    
       <div className="PopularAlbums">
         <span className="Mid">Popular albums</span>
         <div className="FullLign">
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
+          {PopularAlbumsjs.map((popalb) => (
+          <div className="SongContainer" key={popalb.id}>
+            <img src={popalb.image} alt="Img" className="Cover"/>
+            <span className="Title">{popalb.name}</span>
+            <span className="artists">{popalb.desc}</span>
           </div>
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
+          ))}
         </div>
       </div>
+
       <div className="NotMiss">
         <span className="Mid">A ne pas manquer</span>
         <div className="FullLign">
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
+          {PasManquer.map((missjs) => (
+          <div className="SongContainer" key={missjs.id}>
+            <img src={missjs.image} alt="Img" className="Cover"/>
+            <span className="Title">{missjs.name}</span>
+            <span className="artists">{missjs.desc}</span>
           </div>
-          <div className="SongContainer">
-            <img src={firstCover} alt="Img" className="Cover"/>
-            <span className="Title">Daily Mix 1</span>
-            <span className="artists">Mitroglou</span>
-          </div>
+          ))}
         </div>
       </div>
     </div>
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,250 +1,116 @@
-import bell_icon from './bell.png'
-import home_icon from './home.png'
-import like_icon from './like.png'
-import loop_icon from './loop.png'
-import mic_icon from './mic.png'
-import next_icon from './next.png'
-import play_icon from './play.png'
-import pause_icon from './pause.png'
-import plays_icon from './plays.png'
-import prev_icon from './prev.png'
-import search_icon from './search.png'
-import shuffle_icon from './shuffle.png'
-import speaker_icon from './speaker.png'
-import stack_icon from './stack.png'
-import zoom_icon from './zoom.png'
-import plus_icon from './plus.png'
-import arrow_icon from './arrow.png'
-import mini_player_icon from './mini-player.png'
-import queue_icon from './queue.png'
-import volume_icon from './volume.png'
-import arrow_right from './right_arrow.png'
-import arrow_left from './left_arrow.png'
-import spotify_logo from './spotify_logo.png'
-import clock_icon from './clock_icon.png'
-import img1 from './img1.jpg'
-import img2 from './img2.jpg'
-import img3 from './img3.jpg'
-import img4 from './img4.jpg'
-import img5 from './img5.jpg'
-import img6 from './img6.jpg'
-import img7 from './img7.jpg'
-import img8 from './img8.jpg'
-import img9 from './img9.jpg'
-import img10 from './img10.jpg'
-import img11 from './img11.jpg'
-import img12 from './img12.jpg'
-import img13 from './img13.jpg'
-import img14 from './img14.jpg'
-import img15 from './img15.jpg'
-import img16 from './img16.jpg'
-import song1 from  './song1.mp3'
-import song2 from  './song2.mp3'
-import song3 from  './song3.mp3'
+// ../../assets/assets.js
+import bell_icon from './bell.png';
+import home_icon from './home.png';
+import like_icon from './like.png';
+import loop_icon from './loop.png';
+import mic_icon from './mic.png';
+import next_icon from './next.png';
+import play_icon from './play.png';
+import pause_icon from './pause.png';
+import plays_icon from './plays.png';
+import prev_icon from './prev.png';
+import search_icon from './search.png';
+import shuffle_icon from './shuffle.png';
+import speaker_icon from './speaker.png';
+import stack_icon from './stack.png';
+import zoom_icon from './zoom.png';
+import plus_icon from './plus.png';
+import arrow_icon from './arrow.png';
+import mini_player_icon from './mini-player.png';
+import queue_icon from './queue.png';
+import volume_icon from './volume.png';
+import arrow_right from './right_arrow.png';
+import arrow_left from './left_arrow.png';
+import spotify_logo from './spotify_logo.png';
+import clock_icon from './clock_icon.png';
+import img1 from './img1.jpg';
+import img2 from './img2.jpg';
+import img3 from './img3.jpg';
+import img4 from './img4.jpg';
+import img5 from './img5.jpg';
+import img6 from './img6.jpg';
+import img7 from './img7.jpg';
+import img8 from './img8.jpg';
+import img9 from './img9.jpg';
+import img10 from './img10.jpg';
+import img11 from './img11.jpg';
+import img12 from './img12.jpg';
+import img13 from './img13.jpg';
+import img14 from './img14.jpg';
+import img15 from './img15.jpg';
+import img16 from './img16.jpg';
+import song1 from './song1.mp3';
+import song2 from './song2.mp3';
+import song3 from './song3.mp3';
+import art0 from './art0.jpeg';
+import art1 from './art1.jpg';
+import art2 from './art2.jpg';
+import art3 from './art3.jpg';
+import art4 from './art4.jpg';
+import art5 from './art5.jpg';
+import art6 from './art6.jpg';
+import art7 from './art7.jpeg';
+import art8 from './art8.jpeg';
+import alb0 from './alb0.jpeg'
+import alb1 from './alb1.jpeg'
+import alb2 from './alb2.jpeg'
+import alb3 from './alb3.jpeg'
+import alb4 from './alb4.jpeg'
+import alb5 from './alb5.jpeg'
+import alb6 from './alb6.jpeg'
+import alb7 from './alb7.jpeg'
+import alb8 from './alb8.jpeg'
 
 export const assets = {
-    bell_icon,
-    home_icon,
-    like_icon,
-    loop_icon,
-    mic_icon,
-    next_icon,
-    play_icon,
-    plays_icon,
-    prev_icon,
-    search_icon,
-    shuffle_icon,
-    speaker_icon,
-    stack_icon,
-    zoom_icon,
-    plus_icon,
-    arrow_icon,
-    mini_player_icon,
-    volume_icon,
-    queue_icon,
-    pause_icon,
-    arrow_left,
-    arrow_right,
-    spotify_logo,
-    clock_icon
-}
+    bell_icon, home_icon, like_icon, loop_icon, mic_icon, next_icon,
+    play_icon, pause_icon, plays_icon, prev_icon, search_icon, shuffle_icon,
+    speaker_icon, stack_icon, zoom_icon, plus_icon, arrow_icon, mini_player_icon,
+    volume_icon, queue_icon, arrow_left, arrow_right, spotify_logo, clock_icon
+};
 
-export const PopularRadio = [
-    {   
-        id:0,
-        name: "Top 50 Global",
-        image: img8,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#2a4365"
-    },
-    {   
-        id:1,
-        name: "Top 50 India",
-        image: img9,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#22543d"
-    },
-    {   
-        id:2,
-        name: "Trending India",
-        image: img10,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#742a2a"
-    },
-    {   
-        id:3,
-        name: "Trending Global",
-        image: img16,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#44337a"
-    },
-    {   
-        id:4,
-        name: "Mega Hits,",
-        image: img11,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#234e52"
-    },
-    {   
-        id:5,
-        name: "Happy Favorites",
-        image: img15,
-        desc:"Your weekly update of the most played tracks",
-        bgColor:"#744210"
-    }
-]
+export const PopRadiojs = [
+    { id:0, name: "Top 50 Global", image: img8, desc:"Your weekly update of the most played tracks", bgColor:"#2a4365" },
+    { id:1, name: "Top 50 India", image: img9, desc:"Your weekly update of the most played tracks", bgColor:"#22543d" },
+    { id:2, name: "Trending India", image: img10, desc:"Your weekly update of the most played tracks", bgColor:"#742a2a" },
+    { id:3, name: "Trending Global", image: img16, desc:"Your weekly update of the most played tracks", bgColor:"#44337a" },
+    { id:4, name: "Mega Hits", image: img11, desc:"Your weekly update of the most played tracks", bgColor:"#234e52" },
+    { id:5, name: "Happy Favorites", image: img15, desc:"Your weekly update of the most played tracks", bgColor:"#744210" },
+    { id:6, name: "Trending Europe", image: alb1, desc:"Your weekly update of the most played tracks", bgColor:"#744210" },
+    { id:7, name: "Trending China", image: alb2, desc:"Your weekly update of the most played tracks", bgColor:"#744210" },
+    { id:8, name: "Best of Spring", image: alb5, desc:"Your weekly update of the most played tracks", bgColor:"#744210" },
+];
 
-export const PopularAlbums = [
-    {
-        id:0,
-        name: "Song One",
-        image: img1,
-        file:song1,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"3:00"
-    },
-    {
-        id:1,
-        name: "Song Two",
-        image: img2,
-        file:song2,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:20"
-    },
-    {
-        id:2,
-        name: "Song Three",
-        image: img3,
-        file:song3,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:32"
-    },
-    {
-        id:3,
-        name: "Song Four",
-        image: img4,
-        file:song1,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:50"
-    },
-    {
-        id:4,
-        name: "Song Five",
-        image: img5,
-        file:song2,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"3:10"
-    },
-    {
-        id:5,
-        name: "Song Six",
-        image: img14    ,
-        file:song3,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:45"
-    },
-    {
-        id:6,
-        name: "Song Seven",
-        image: img7,
-        file:song1,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:18"
-    },
-    {
-        id:7,
-        name: "Song Eight",
-        image: img12,
-        file:song2,
-        desc:"Put a smile on your face with these happy tunes",
-        duration:"2:35"
-    }
-]
+export const PopularAlbumsjs = [
+    { id:0, name: "Song One", image: img1, file:song1, desc:"Put a smile on your face with these happy tunes", duration:"3:00" },
+    { id:1, name: "Song Two", image: img2, file:song2, desc:"Put a smile on your face with these happy tunes", duration:"2:20" },
+    { id:2, name: "Song Three", image: img3, file:song3, desc:"Put a smile on your face with these happy tunes", duration:"2:32" },
+    { id:3, name: "Song Four", image: img4, file:song1, desc:"Put a smile on your face with these happy tunes", duration:"2:50" },
+    { id:4, name: "Song Five", image: img5, file:song2, desc:"Put a smile on your face with these happy tunes", duration:"3:10" },
+    { id:5, name: "Song Six", image: img14, file:song3, desc:"Put a smile on your face with these happy tunes", duration:"2:45" },
+    { id:6, name: "Song Seven", image: img7, file:song1, desc:"Put a smile on your face with these happy tunes", duration:"2:18" },
+    { id:7, name: "Song Eight", image: img12, file:song2, desc:"Put a smile on your face with these happy tunes", duration:"2:35" }
+];
 
 export const PasManquer = [
-    {
-        id:0,
-        name: "First",
-        image: img1,
-        file: song1,
-        desc: "Do not miss this one fam",
-        duration: "2:18",
-    }
-]
+    { id:0, name: "Pyta", image: alb0, file: song1, desc: "Do not miss this one fam", duration: "2:18" },
+    { id:0, name: "Lilx", image: alb1, file: song1, desc: "Summer", duration: "2:18" },
+    { id:0, name: "zenn.", image: alb2, file: song1, desc: "Heavy thought", duration: "2:18" },
+    { id:0, name: "almando", image: alb3, file: song1, desc: "Sad", duration: "2:18" },
+    { id:0, name: "Zyfa", image: alb4, file: song1, desc: "Winter", duration: "2:18" },
+    { id:0, name: "death.", image: alb5, file: song1, desc: "Just vibe", duration: "2:18" },
+    { id:0, name: "Misery", image: alb6, file: song1, desc: "You want more ?", duration: "2:18" },
+    { id:0, name: "Celestia", image: alb7, file: song1, desc: "Pure joy", duration: "2:18" },
+    { id:0, name: "Mneee", image: alb8, file: song1, desc: "Zennnn", duration: "2:18" },
+];
 
-export const Artists = [
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art0,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art1,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art2,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art3,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art4,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art5,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art6,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art7,
-    },
-    {
-        id:0,
-        name: "name",
-        desc: "artist",
-        image: art8,
-    },
-]
+export const Artistsjs = [
+    { id:0, name: "Mewww", desc: "artist", image: art0 },
+    { id:1, name: "rosaria.", desc: "artist", image: art1 },
+    { id:2, name: "woa", desc: "artist", image: art2 },
+    { id:3, name: "mrekk", desc: "artist", image: art3 },
+    { id:4, name: "tuna", desc: "artist", image: art4 },
+    { id:5, name: "lilo", desc: "artist", image: art5 },
+    { id:6, name: "damso", desc: "artist", image: art6 },
+    { id:7, name: "fio.", desc: "artist", image: art7 },
+    { id:8, name: "armada", desc: "artist", image: art8 }
+];
